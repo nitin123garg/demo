0731a2d89580243756ca7ea906b6d59b5b35edbc
nitin123garg <95nitingarg@gmail.com>
2020-09-12 09:53:49 -0700

    my first version of sc.py

---
+++ sc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+name = input("enter your name")
+
+import os
+
+os.system("clear")
+
+print("\n\n\n\n")
+print("-"*40)
+print("-"*40)
+
+
+print (f"welcome user,{name}")
+
+print("\n\n\n")
+print("-"*40)
+print("-"*40)
